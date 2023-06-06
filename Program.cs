faa8d3524c0c404d06999d418d8151ac01e67451
aluno <aluno@A2724>
2023-06-05 21:16:44 -0300

    FOR Comentado

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ int num;
 Console.WriteLine("Digite um numero intero"); //interface para o usuario
 num = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);//amazena o numero informado pelo usuario
 
-Console.WriteLine("Os primeiros " + num + " numeros pares sao:\n");
+Console.WriteLine("Os primeiros " + num + " numeros pares sao:\n");//interface para o usuario informando o que ira acontecer
 
-for (int i= 2; i <= num * 2; i+=2)
+for (int i= 2; i <= num * 2; i+=2) //para cada vez que o codigo for executado, i que eh o parametro da repeticao sera adicionado de mais 2
 {
     Console.WriteLine(i + "\n"); //mostra o valor de i na tela fazendo contagem até o numero que o usuario inseriu
 }
